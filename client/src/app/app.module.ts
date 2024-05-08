aae34be3f8eeb94c6f1f80f1f272739673ea6f44
Louis Pate <lfpate11@gmail.com>
2024-05-08 19:43:28 -0400

    eslint is linked with prettier and now working when running es lint command

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TooltipModule.forRoot()
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TooltipModule.forRoot()],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
