785a363be485bb2e9e1d2c570a5f44a67657a55b
Louis Pate <lfpate11@gmail.com>
2024-05-09 16:32:11 -0400

    cleaned up back component with prettier and added more typing for better readibility and better practice

--- client/src/app/back/back.component.ts
+++ client/src/app/back/back.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BsModalService, BsModalRef} from 'ngx-bootstrap/modal';
+import {Observable} from 'rxjs';
 
 interface BackItem {
   back_id: number;
@@ -9,6 +10,11 @@ interface BackItem {
   reps: string;
   pr: string;
   prev: string;
+}
+
+interface BackResponse {
+  success: boolean;
+  message: string;
 }
 
 @Component({
@@ -34,28 +40,58 @@ export class BackComponent implements OnInit {
     this.getAllBacks();
   }
 
-  onSubmit() {
+  onSubmit(): void {
     this.createBack().subscribe({
       next: () => (window.location.href = '/back'), //redirecting to back page after submission
       error: (error) => console.error(error),
     });
   }
 
+  onUpdate(
+    id: number,
+    description: string,
+    rounds: string,
+    reps: string,
+    pr: string,
+    prev: string,
+  ): void {
+    this.updateBack(id, description, rounds, reps, pr, prev).subscribe({
+      next: () => {
+        window.location.href = '/back'; // Redirecting to the back page after update
+      },
+      error: (error) => {
+        console.error(error.message); // Handling errors
+      },
+    });
+  }
+
+  onDelete(id: number): void {
+    this.deleteBack(id).subscribe({
+      next: () => {
+        this.back = this.back.filter((back) => back.back_id !== id);
+        window.location.href = '/back'; //returning to back page to load correct description
+      },
+      error: (error: Error) => {
+        console.error((error as Error).message);
+      },
+    });
+  }
+
   // GET all Back exercises //
-  async getAllBacks() {
-    this.http.get<BackItem[]>('http://localhost:9000/back').subscribe(
-      (data) => {
+  getAllBacks() {
+    this.http.get<BackItem[]>('http://localhost:9000/back').subscribe({
+      next: (data) => {
         this.back = data;
       },
-      (error) => {
-        console.error(error.messages);
+      error: (error) => {
+        console.error(error.message);
       },
-    );
+    });
   }
 
   // CREATE a Back exercise //
 
-  createBack() {
+  createBack(): Observable<BackResponse> {
     const body = {
       description: this.description,
       rounds: this.rounds,
@@ -63,7 +99,7 @@ export class BackComponent implements OnInit {
       pr: this.pr,
       prev: this.prev,
     };
-    return this.http.post('http://localhost:9000/back', body);
+    return this.http.post<BackResponse>('http://localhost:9000/back', body);
   }
 
   //EDIT a Back exercise //
@@ -74,33 +110,16 @@ export class BackComponent implements OnInit {
     reps: string,
     pr: string,
     prev: string,
-  ): void {
+  ): Observable<void> {
     const body = {description, rounds, reps, pr, prev};
-    this.http
-      .put(`http://localhost:9000/back/${id}`, body, {
-        headers: {'Content-Type': 'application/json'},
-      })
-      .subscribe(
-        () => {
-          window.location.href = '/back'; //returning to back page to load correct description
-        },
-        (error) => {
-          console.error(error.message);
-        },
-      );
+    return this.http.put<void>(`http://localhost:9000/back/${id}`, body, {
+      headers: {'Content-Type': 'application/json'},
+    });
   }
 
   // DELETE a Back exercise //
-  async deleteBack(id: number) {
-    this.http.delete(`http://localhost:9000/back/${id}`).subscribe(
-      () => {
-        this.back = this.back.filter((back) => back.back_id !== id);
-        window.location.href = '/back'; //returning to back page to load correct description
-      },
-      (error) => {
-        console.error((error as Error).message);
-      },
-    );
+  deleteBack(id: number): Observable<void> {
+    return this.http.delete<void>(`http://localhost:9000/back/${id}`);
   }
 
   // Open Modal //
