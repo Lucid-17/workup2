acfc6d079aa2c30264f81359d3f25a9bfbad69a5
Louis Pate <lfpate11@gmail.com>
2024-05-17 18:29:21 -0400

    added 404.html file to docs folder

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-7AMKWGYC.mjs";import"./chunk-J3EFSXBI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-NVBTW2BR.mjs";import"./chunk-J3EFSXBI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
