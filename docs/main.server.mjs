1e790f3f4483eca94a0815d27c82dac2d3fb4e72
Louis Pate <lfpate11@gmail.com>
2024-05-17 19:18:23 -0400

    added conditional statements in component for redirection

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NVBTW2BR.mjs";import"./chunk-J3EFSXBI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-VXVMXNVT.mjs";import"./chunk-J3EFSXBI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
